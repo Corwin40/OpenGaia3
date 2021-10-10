c7cb345c28482a7664012a7f978e86fc5c12ae80
Xavier BURKE <xavier.burke@openpixl.fr>
2021-10-10 11:49:04 +0200

    Développement des besoins clients
    - Collaborateur : ajout sur une structure d'un membre supplémentaire.
    - envoie de mail selone dates des évènements
    
    - envoie de mail à l'ensemble

---
+++ src/Controller/Admin/CollaboratorController.php
@@ -0,0 +1,94 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Entity\Admin\Collaborator;
+use App\Form\Admin\CollaboratorType;
+use App\Repository\Admin\CollaboratorRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/admin/collaborator")
+ */
+class CollaboratorController extends AbstractController
+{
+    /**
+     * @Route("/", name="admin_collaborator_index", methods={"GET"})
+     */
+    public function index(CollaboratorRepository $collaboratorRepository): Response
+    {
+        return $this->render('admin/collaborator/index.html.twig', [
+            'collaborators' => $collaboratorRepository->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/new", name="admin_collaborator_new", methods={"GET","POST"})
+     */
+    public function new(Request $request): Response
+    {
+        $collaborator = new Collaborator();
+        $form = $this->createForm(CollaboratorType::class, $collaborator);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->persist($collaborator);
+            $entityManager->flush();
+
+            return $this->redirectToRoute('admin_collaborator_index');
+        }
+
+        return $this->render('admin/collaborator/new.html.twig', [
+            'collaborator' => $collaborator,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="admin_collaborator_show", methods={"GET"})
+     */
+    public function show(Collaborator $collaborator): Response
+    {
+        return $this->render('admin/collaborator/show.html.twig', [
+            'collaborator' => $collaborator,
+        ]);
+    }
+
+    /**
+     * @Route("/{id}/edit", name="admin_collaborator_edit", methods={"GET","POST"})
+     */
+    public function edit(Request $request, Collaborator $collaborator): Response
+    {
+        $form = $this->createForm(CollaboratorType::class, $collaborator);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $this->getDoctrine()->getManager()->flush();
+
+            return $this->redirectToRoute('admin_collaborator_index');
+        }
+
+        return $this->render('admin/collaborator/edit.html.twig', [
+            'collaborator' => $collaborator,
+            'form' => $form->createView(),
+        ]);
+    }
+
+    /**
+     * @Route("/{id}", name="admin_collaborator_delete", methods={"POST"})
+     */
+    public function delete(Request $request, Collaborator $collaborator): Response
+    {
+        if ($this->isCsrfTokenValid('delete'.$collaborator->getId(), $request->request->get('_token'))) {
+            $entityManager = $this->getDoctrine()->getManager();
+            $entityManager->remove($collaborator);
+            $entityManager->flush();
+        }
+
+        return $this->redirectToRoute('admin_collaborator_index');
+    }
+}
---
+++ src/Form/Admin/CollaboratorType.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace App\Form\Admin;
+
+use App\Entity\Admin\Collaborator;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichImageType;
+
+class CollaboratorType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+            ->add('firstName')
+            ->add('lastName')
+            ->add('email')
+            ->add('avatarFile', VichImageType::class, [
+                'required' => false,
+                'allow_delete' => true,
+                'delete_label' => 'Supprimer',
+                'download_label' => 'Télecharger',
+            ])
+            ->add('phoneDesk')
+            ->add('phoneGsm')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => Collaborator::class,
+        ]);
+    }
+}
